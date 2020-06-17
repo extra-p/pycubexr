afd83ebbc578904046109fae240343fa8f842ff9
Marcus Ritter <ritter.marcus93@gmail.com>
2020-06-17 15:18:03 +0200

    added data for test cases and updated setup file

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from glob import glob
 from pycubex_parser import CubexParser
 from pycubex_parser.utils.exceptions import MissingMetricError
 
+#"data/blast.p64.r1/profile.cubex"
+#'assets/*.r1'
 
 def main():
-    for folder in sorted(glob('assets/*.r1')):
+    for folder in sorted(glob('data/kripke.p8.d2.g32.r1')):
         print('-' * 99)
         print(folder)
 
@@ -14,10 +16,22 @@ def main():
         with CubexParser(profile) as parsed:
             parsed.print_calltree()
 
+            # extracting all the metrics
+            # should extract only the ones we can actually model
+            # or offer the user to choose them...
             for metric in parsed.get_metrics():
                 try:
                     metric_values = parsed.get_metric_values(metric=metric)
+                    # with the cnode_indices I can manipulate the region that is chosen
+                    # should put for here to extract all of them
                     cnode = parsed.get_cnode(metric_values.cnode_indices[0])
+                    #debug
+                    #print(metric_values.cnode_indices)
+                    #print("node values:",metric_values.cnode_values(cnode.id))
+                    #print("number node values:",len(metric_values.cnode_values(cnode.id)))
+
+                    # the more processes the more values per node, like this shows only 5 of them
+                    # here we will do some magic with selecting only certain values based on that clustering algorithm
                     cnode_values = metric_values.cnode_values(cnode.id)[:5]
                     region = parsed.get_region(cnode)
                     print('\t' + '-' * 100)
--- setup.py
+++ setup.py
@@ -4,18 +4,19 @@ with open("README.md") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pycubex_parser",
+    name="py-cube",
     version="0.0.1",
-    author="David Gengenbach",
-    author_email="info@davidgengenbach.de",
-    description="Parser for *.cubex files",
+    author="Extra-P project",
+    author_email="extra-p@lists.parallel.informatik.tu-darmstadt.de",
+    description="Python Cube file reader for the Cube 4.x file format",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/davidgengenbach/pycubex_parser",
+    url="https://github.com/extra-p/py-cube",
     packages=setuptools.find_packages(),
     classifiers=[
+        "Development Status :: 3 - Alpha",
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.5',
