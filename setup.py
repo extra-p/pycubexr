6698d19a507211569ccb749f23a0b1ce4b721aa0
Alexander Geiß <alexander.geiss1@tu-darmstadt.de>
2024-12-04 11:03:56 +0100

    Set version to 1.2.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as fh:
 
 setuptools.setup(
     name="pycubexr",
-    version="1.2.2",
+    version="1.2.3",
     author="Extra-P project",
     author_email="extra-p@lists.parallel.informatik.tu-darmstadt.de",
     description="pyCubexR is a Python package for reading the Cube4 file format.",
