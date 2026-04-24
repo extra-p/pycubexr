28ad6db2c67d6bc7c87315041c827a60fbdc91c3
Alexander Geiß <alexander.geiss1@tu-darmstadt.de>
2026-04-24 18:34:22 +0200

    Public test for Cube files with checksum error

--- test/test_cube_with_bug.py
+++ test/test_cube_with_bug.py
@@ -1,6 +1,6 @@
-import unittest
 from pathlib import Path
-from unittest import SkipTest
+
+import unittest
 
 from pycubexr import CubexParser
 
@@ -8,13 +8,10 @@ from pycubexr import CubexParser
 class TestCube4_8WithBug(unittest.TestCase):
 
     def test_load(self):
-        try:
-            cubex_file_path = Path("../data/cube_samples/cube4.8_with_checksum_bug.cubex").resolve()
-            with self.assertWarnsRegex(UserWarning, "checksum"):
-                with CubexParser(cubex_file_path) as cube_file:
-                    cube_file.all_metrics()
-        except FileNotFoundError as err:
-            raise SkipTest("Required test file was not found.") from err
+        cubex_file_path = Path("../data/bt-mz_sum.p2.r1/cube4.8_with_checksum_bug.cubex").resolve()
+        with self.assertWarnsRegex(UserWarning, "checksum"):
+            with CubexParser(cubex_file_path) as cube_file:
+                cube_file.all_metrics()
 
 
 if __name__ == '__main__':
